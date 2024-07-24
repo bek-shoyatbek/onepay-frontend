59036feb2109c2a7dba6b9f1c1df3a87fdfbd107
bek <shoyatbekov03032003@gmail.com>
2024-07-24 18:12:38 +0500

    space added right and left side of payment type choosing buttons

--- src/constants/payment-services/payment-providers.ts
+++ src/constants/payment-services/payment-providers.ts
@@ -1,18 +1,30 @@
+import uzum from "../../assets/uzum.png";
+import payme from "../../assets/payme.png";
+import click from "../../assets/click.png";
 export const paymentProviders = [
   {
     id: "uzum",
     name: "uzum",
-    icon: "https://drive.google.com/thumbnail?id=1h8LtyzeO4eqK09y7yOCE6RAAH4IY1k57",
+    // icon: "https://drive.google.com/thumbnail?id=1h8LtyzeO4eqK09y7yOCE6RAAH4IY1k57",
+    icon: uzum,
   },
   {
     id: "payme",
     name: "payme",
-    icon: "https://api.logobank.uz/media/logos_png/payme-01.png",
+    // icon: "https://api.logobank.uz/media/logos_png/payme-01.png",
+    icon: payme,
   },
   {
     id: "click",
     name: "click",
-    icon: "https://api.logobank.uz/media/logos_png/Click-01.png",
+    // icon: "https://api.logobank.uz/media/logos_png/Click-01.png",
+    icon: click,
+  },
+  {
+    id: "payme1",
+    name: "payme1",
+    // icon: "https://api.logobank.uz/media/logos_png/payme-01.png",
+    icon: payme,
   },
 ];
 
